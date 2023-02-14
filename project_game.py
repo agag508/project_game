509a5b228c1d78335b9708d05eaa77a6552c96b1
Aga <agag508@gmail.com>
2023-02-14 07:35:15 +0000

    fixing bug, improving the loop

--- project_game.py
+++ project_game.py
@@ -23,22 +23,34 @@ else:
     print("Thanks! You are going to play with {}".format(player_one_symbol))
 
 
-def player_one():
-    position_index = int(input("Hi! please choose the position you want to place your symbol"))
-    while position_index not in range(9):
-        position_index = int("Please choose a valid number between 0 and 8")
+position_index = int(input("Please choose the position you want to place your symbol"))
+while position_index not in range(9):
+    position_index = int(input("Please choose a valid number between 0 and 8"))
+else:
+    if position_index in [0, 1, 2]:
+        line1[position_index] = [player_one_symbol]
+        """print( line1[position_index]) prints individual cell)"""
+    elif position_index in [3, 4, 5]:
+        line2[position_index - 3] = [player_one_symbol]
+    else:
+        line3[position_index - 6] = [player_one_symbol]
 
-        if position_index in [0, 1, 2]:
-            line1[position_index] = [player_one_symbol]
-            print(line1[position_index])
-        elif position_index in [3, 4, 5]:
-            line2[position_index - 3] = [player_one_symbol]
-            print(line2[position_index - 3])
-        else:
-            line3[position_index - 6] = [player_one_symbol]
-            print(line3[position_index - 6])
-
-
-player_one()
 
 board()
+
+"""
+print("something ")
+def keep_playing():
+    yes_no = input("Do you want to keep playing, if so press y, if no choose n")
+    assert yes_no in ['y', 'n', 'Y', 'N']:
+    if yes_no in ['y','Y']:
+            print("lets play again")
+            
+    else:
+        raise ValueError
+
+
+
+keep_playing()
+
+print('something after'):"""
