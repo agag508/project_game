9291e40796fb9fae0f86af409e8739e52365e83c
Aga <agag508@gmail.com>
2023-02-15 07:18:07 +0000

    new function to play again

--- project_game.py
+++ project_game.py
@@ -15,42 +15,56 @@ def board():
 
 board()
 
-player_one_symbol = input('Please choose o or x')
 
-while player_one_symbol not in ['o', 'x']:
-    player_one_symbol = input('Please choose a valid symbol o or x')
-else:
-    print("Thanks! You are going to play with {}".format(player_one_symbol))
+def symbol_pick():
+    player_one_symbol = input('Please choose o or x')
 
-
-position_index = int(input("Please choose the position you want to place your symbol"))
-while position_index not in range(9):
-    position_index = int(input("Please choose a valid number between 0 and 8"))
-else:
-    if position_index in [0, 1, 2]:
-        line1[position_index] = [player_one_symbol]
-        """print( line1[position_index]) prints individual cell)"""
-    elif position_index in [3, 4, 5]:
-        line2[position_index - 3] = [player_one_symbol]
+    while player_one_symbol not in ['o', 'x']:
+        player_one_symbol = input('Please choose a valid symbol o or x')
     else:
-        line3[position_index - 6] = [player_one_symbol]
+        print("Thanks! You are going to play with {}".format(player_one_symbol))
+        return player_one_symbol
 
 
+chosen_symbol = int
+chosen_symbol = symbol_pick()
+
+
+def game_on():
+    position_index = int(input("Please choose the position you want to place your symbol"))
+    while position_index not in range(9):
+        position_index = int(input("Please choose a valid number between 0 and 8"))
+    else:
+        if position_index in [0, 1, 2]:
+            line1[position_index] = [chosen_symbol]
+            """print( line1[position_index]) prints individual cell)"""
+        elif position_index in [3, 4, 5]:
+            line2[position_index - 3] = [chosen_symbol]
+        else:
+            line3[position_index - 6] = [chosen_symbol]
+
+
+game_on()
 board()
 
-"""
-print("something ")
 def keep_playing():
-    yes_no = input("Do you want to keep playing, if so press y, if no choose n")
-    assert yes_no in ['y', 'n', 'Y', 'N']:
-    if yes_no in ['y','Y']:
-            print("lets play again")
-            
+    yes_no = input("Do you want to keep playing? If so press y, if no choose n")
+    while yes_no not in ['y', 'n', 'Y', 'N']:
+        yes_no = input("Invalid input, please choose y or n.")
     else:
-        raise ValueError
+       try:
+           yes_no in ['y','Y']
+
+       except yes_no =='n':
+           raise ValueError
+       else:
+           return symbol_pick()
+
+
+
 
 
 
 keep_playing()
-
-print('something after'):"""
+game_on()
+board()
