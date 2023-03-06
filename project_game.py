1198bca3f08b79e67c3d1ff178749a3312260174
Aga <agag508@gmail.com>
2023-03-06 20:28:06 +0000

    a big reorganisation of the whole game, using list and set of str to display board, adding random player

--- project_game.py
+++ project_game.py
@@ -1,64 +1,55 @@
-board_lines = []
+import random
 
-line1 = [[0], [1], [2]]
-line2 = [[3], [4], [5]]
-line3 = [[6], [7], [8]]
-
-board_lines = [line1, line2, line3]
+board_list = ['0','1','2','3','4','5','6','7','8']
 
 
-def board():
-    for line in board_lines:
-        print(line)
-    return board_lines
+def print_board(board):
+    print(' --- --- ---')
+    print('| ' + board[0] + ' | ' + board[1] + ' | ' + board[2]+ ' | ')
+    print(' --- --- ---')
+    print('| ' + board[3] + ' | ' + board[4] + ' | ' + board[5]+ ' | ')
+    print(' --- --- ---')
+    print('| ' + board[6] + ' | ' + board[7] + ' | ' + board[8]+ ' | ')
+    print(' --- --- ---')
+    return board
 
 
-def tic_tac():
-    board()
+print_board(board_list)
+
+your_sign = input("\nWhat symbol do you want to play with? Choose O or X")
+opponent_sign = str
+while your_sign not in ['o', 'O', 'x', 'X']:
+    your_sign = input("Please choose valid symbol. Choose O or X")
+
+else:
+    if your_sign == 'o' or your_sign == 'O':
+        opponent_sign = 'X'
+
+    else:
+        opponent_sign = 'O'
 
 
-    def symbol_pick():
-        player_one_symbol = input('Please choose o or x')
 
-        while player_one_symbol not in ['o', 'x']:
-            player_one_symbol = input('Please choose a valid symbol o or x')
+def game_on():
+    free_indexes_tracker = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    while len(free_indexes_tracker) > 0:
+
+        position_index = int(input("Please choose the position you want to place your symbol"))
+        while position_index not in free_indexes_tracker:
+            position_index = int(input("Please choose a valid number from the list {}".format(free_indexes_tracker)))
         else:
-            print("Thanks! You are going to play with {}".format(player_one_symbol))
-            return player_one_symbol
-
-
-    chosen_symbol = int
-    chosen_symbol = symbol_pick()
-
-
-    def game_on():
-
-        position_index = input("Please choose the position you want to place your symbol")
-
-        while position_index not in ['1', '2', '3', '4', '5', '6', '7', '8']:
-
-            position_index = input("Please choose a valid number between 0 and 8")
-
-        else:
-            pos = int(position_index)
-
-            if pos in [0, 1, 2]:
-                line1[pos] = [chosen_symbol]
-                """print( line1[position_index]) prints individual cell)"""
-            elif pos in [3, 4, 5]:
-                line2[pos - 3] = [chosen_symbol]
-            else:
-                line3[pos - 6] = [chosen_symbol]
-
-
-    game_on()
-    board()
+            board_list[position_index] = your_sign
+            free_indexes_tracker.pop(free_indexes_tracker.index(position_index))  # removing position from the list
+            random_position_from_tracker = random.choice(free_indexes_tracker)  # picking a random position for opponent
+            board_list[random_position_from_tracker] = opponent_sign
+            free_indexes_tracker.pop(free_indexes_tracker.index(random_position_from_tracker))
+            print_board(board_list)
 
     def keep_playing():
         yes_no = input("Do you want to keep playing? If so press y, if no choose n")
         while yes_no == 'y':
             print('Ok, lets play')
-            tic_tac()
+            game_on()
         else:
             try:
                 while yes_no == 'n':
@@ -66,13 +57,47 @@ def tic_tac():
 
                     break
             except yes_no not in ['n']:
+
                 raise ValueError
                 yes_no = input("Wrong value, If yes press y, if no choose n")
 
-    keep_playing()
+            while len(free_indexes_tracker) <= 4:
+                def win():
+                    if board_list[0] == board_list[1] == board_list[2]:
+                        return " Person playing with {} won!".format(board_list[0])
+                        print(keep_playing())
+                    elif board_list[3] == board_list[4] == board_list[5]:
+                        return " Person playing with {} won!".format(board_list[4])
+                        print(keep_playing())
+                    elif board_list[7] == board_list[8] == board_list[6]:
+                        return " Person playing with {} won!".format(board_list[6])
+                        print(keep_playing())
+                    elif board_list[0] == board_list[3] == board_list[6]:
+                        return " Person playing with {} won!".format(board_list[0])
+                        print(keep_playing())
+                    elif board_list[1] == board_list[4] == board_list[7]:
+                        return " Person playing with {} won!".format(board_list[1])
+                        print(keep_playing())
+                    elif board_list[2] == board_list[5] == board_list[8]:
+                        return " Person playing with {} won!".format(board_list[5])
+                        print(keep_playing())
+                    else:
+                        while False:
+                            continue
+                print(win())
 
 
-tic_tac()
+
+
+game_on()
+
+print_board(board_list)
+
+
+
+
+
+
 
 
 
