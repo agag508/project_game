667c70dd16ef9c9ccd49989cccf8143dd6985e51
Aga <agag508@gmail.com>
2023-03-08 07:48:56 +0000

    Added colours, improved keep playing and win functions, changed flow of the game

--- project_game.py
+++ project_game.py
@@ -1,8 +1,20 @@
 import random
 
+
+def p_red(obj):
+    return "\033[91m {}\033[00m" .format(obj)
+
+
+def p_green(obj):
+    return "\033[92m {}\033[00m" .format(obj)
+
+
+print('\n'*48)
+
+
 def play():
 
-    board_list = ['0','1','2','3','4','5','6','7','8']
+    board_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 
     def print_board(board):
         print(' --- --- ---')
@@ -14,91 +26,94 @@ def play():
         print(' --- --- ---')
         return board
 
-
     print_board(board_list)
 
-
-    your_sign = input("\nWhat symbol do you want to play with? Choose O or X")
+    your_sign = input("\nWhat symbol do you want to play with? Choose o or x")
     opponent_sign = str
-    while your_sign not in ['o', 'O', 'x', 'X']:
-        your_sign = input("Please choose valid symbol. Choose O or X")
 
+    while your_sign not in ['o', 'x']:
+        your_sign = input("Please choose valid symbol. Choose o or x")
     else:
-        if your_sign == 'o' or your_sign == 'O':
-            opponent_sign = 'X'
-
+        if your_sign == 'o':
+            opponent_sign = 'x'
         else:
-            opponent_sign = 'O'
-
+            opponent_sign = 'o'
 
     def keep_playing():
+
         yes_no = input("Do you want to keep playing? If so press y, if no choose n")
         while yes_no == 'y':
             print('Ok, lets play')
-
-
-
+            print('\n'*20)
             play()
         else:
-            try:
-                while yes_no == 'n':
-                    print('Nice game! See you next time')
+            while yes_no != 'n':
+                yes_no = input("Please choose y if you want to keep playing or no if you want to quit.")
+                continue
+            else:
+                print('Nice game! See you next time')
+                quit()
 
-                    break
-            except yes_no not in ['n']:
+    def win():
 
-                raise ValueError
-                yes_no = input("Wrong value, If yes press y, if no choose n")
+        if board_list[0] == board_list[1] == board_list[2]:
+            return " Person playing with {} won!".format(board_list[0])
 
+        elif board_list[3] == board_list[4] == board_list[5]:
+            return " Person playing with {} won!".format(board_list[4])
+
+        elif board_list[7] == board_list[8] == board_list[6]:
+            return " Person playing with {} won!".format(board_list[6])
+
+        elif board_list[0] == board_list[3] == board_list[6]:
+            return " Person playing with {} won!".format(board_list[0])
+
+        elif board_list[1] == board_list[4] == board_list[7]:
+            return " Person playing with {} won!".format(board_list[1])
+
+        elif board_list[2] == board_list[5] == board_list[8]:
+            return " Person playing with {} won!".format(board_list[5])
+        elif board_list[0] == board_list[4] == board_list[8]:
+            return " Person playing with {} won!".format(board_list[4])
+        elif board_list[2] == board_list[6] == board_list[4]:
+            return " Person playing with {} won!".format(board_list[4])
+        else:
+            while False:
+                continue
 
     def game_on():
 
         free_indexes_tracker = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-        while len(free_indexes_tracker) > 0:
+        while len(free_indexes_tracker) >= 0:
 
             position_index = int(input("Please choose the position you want to place your symbol"))
             while position_index not in free_indexes_tracker:
                 position_index = int(input("Please choose a valid number from the list {}".format(free_indexes_tracker)))
             else:
-                board_list[position_index] = your_sign
+                board_list[position_index] = p_green(your_sign)
                 free_indexes_tracker.pop(free_indexes_tracker.index(position_index))  # removing position from the list
                 random_position_from_tracker = random.choice(free_indexes_tracker)  # picking a random position for opponent
-                board_list[random_position_from_tracker] = opponent_sign
+                board_list[random_position_from_tracker] = p_red(opponent_sign)
                 free_indexes_tracker.pop(free_indexes_tracker.index(random_position_from_tracker))
                 print_board(board_list)
 
                 if len(free_indexes_tracker) <= 4:
-                    def win():
-                        if board_list[0] == board_list[1] == board_list[2]:
-                            return " Person playing with {} won!".format(board_list[0])
 
-                        elif board_list[3] == board_list[4] == board_list[5]:
-                            return " Person playing with {} won!".format(board_list[4])
-
-                        elif board_list[7] == board_list[8] == board_list[6]:
-                            return " Person playing with {} won!".format(board_list[6])
-
-                        elif board_list[0] == board_list[3] == board_list[6]:
-                            return " Person playing with {} won!".format(board_list[0])
-
-                        elif board_list[1] == board_list[4] == board_list[7]:
-                            return " Person playing with {} won!".format(board_list[1])
-
-                        elif board_list[2] == board_list[5] == board_list[8]:
-                            return " Person playing with {} won!".format(board_list[5])
-
-                        else:
-                            while False:
-                                continue
                     print(win())
                     if win():
                         keep_playing()
+
+                if len(free_indexes_tracker) == 0 and not win():
+                    print("It is a draw, try your luck next time!")
+                    keep_playing()
 
     print_board(board_list)
     game_on()
 
 play()
+
+
 
 
 
