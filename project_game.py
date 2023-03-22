7a9155c289482e9d8310e2574716d7c386858fcb
Aga <agag508@gmail.com>
2023-03-22 07:04:31 +0000

    Making code more efficient, swapping str.format() for f'strings etc

--- project_game.py
+++ project_game.py
@@ -1,23 +1,38 @@
+"""
+A simple TIC TAC TOE inspired game for two players.
+"""
 import random
+
+"""
+ Function changing string to red
+"""
 
 
 def p_red(obj):
     return "\033[91m{}\033[00m".format(obj)
 
 
+"""
+# Function changing string colour to green
+"""
+
+
 def p_green(obj):
     return "\033[92m{}\033[00m".format(obj)
 
 
-print('\n'*48)
+print('\n'*48)  # Clearing the screen
+
+"""
+Main function of the game.
+"""
 
 
 def play():
-
     board_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 
     def print_board(board):
-        print(('  ---  ---  --- \n| {} | {} | {} | \n  ---  ---  --- \n| {} | {} | {} | \n').format(board[0],board[1], board[2],board[3],board[4], board[5]))
+
         print(' --- --- ---')
         print('| ' + board[0] + ' | ' + board[1] + ' | ' + board[2]+ ' | ')
         print(' --- --- ---')
@@ -42,14 +57,16 @@ def play():
 
     def keep_playing():
 
-        yes_no = input("Do you want to keep playing? If so press y, if no choose n")
+        yes_no = input("Do you want to keep playing? If so press y,"
+                       " if no choose n")
         while yes_no == 'y':
             print('Ok, lets play')
             print('\n'*20)
             play()
         else:
             while yes_no != 'n':
-                yes_no = input("Please choose y if you want to keep playing or no if you want to quit.")
+                yes_no = input("Please choose y if you want to keep "
+                               "playing or no if you want to quit.")
                 continue
             else:
                 print('Nice game! See you next time')
@@ -58,26 +75,26 @@ def play():
     def win():
 
         if board_list[0] == board_list[1] == board_list[2]:
-            return " Person playing with {} won!".format(board_list[0])
+            return f" Person playing with {board_list[0]} won!"
 
-        elif board_list[3] == board_list[4] == board_list[5]:
-            return " Person playing with {} won!".format(board_list[4])
+        if board_list[3] == board_list[4] == board_list[5]:
+            return f" Person playing with {board_list[4]} won!"
 
-        elif board_list[7] == board_list[8] == board_list[6]:
-            return " Person playing with {} won!".format(board_list[6])
+        if board_list[7] == board_list[8] == board_list[6]:
+            return f" Person playing with {board_list[6]} won!"
 
-        elif board_list[0] == board_list[3] == board_list[6]:
-            return " Person playing with {} won!".format(board_list[0])
+        if board_list[0] == board_list[3] == board_list[6]:
+            return f" Person playing with {board_list[0]} won!"
 
-        elif board_list[1] == board_list[4] == board_list[7]:
-            return " Person playing with {} won!".format(board_list[1])
+        if board_list[1] == board_list[4] == board_list[7]:
+            return f" Person playing with {format(board_list[1])} won!"
 
-        elif board_list[2] == board_list[5] == board_list[8]:
-            return " Person playing with {} won!".format(board_list[5])
-        elif board_list[0] == board_list[4] == board_list[8]:
-            return " Person playing with {} won!".format(board_list[4])
-        elif board_list[2] == board_list[6] == board_list[4]:
-            return " Person playing with {} won!".format(board_list[4])
+        if board_list[2] == board_list[5] == board_list[8]:
+            return f" Person playing with {board_list[5]} won!"
+        if board_list[0] == board_list[4] == board_list[8]:
+            return f" Person playing with {board_list[4]} won!"
+        if board_list[2] == board_list[6] == board_list[4]:
+            return f" Person playing with {board_list[4]} won!"
         else:
             while False:
                 continue
@@ -88,12 +105,15 @@ def play():
 
         while len(free_indexes_tracker) >= 0:
 
-            position_index = int(input("Please choose the position you want to place your symbol"))
+            position_index = int(input("Please choose the position "
+                                       "you want to place your symbol"))
             while position_index not in free_indexes_tracker:
-                position_index = int(input("Please choose a valid number from the list {}".format(free_indexes_tracker)))
+                position_index = int(input(f"Please choose a valid number"
+                                           f" from the list {free_indexes_tracker}"))
             else:
                 board_list[position_index] = p_green(your_sign)
-                free_indexes_tracker.pop(free_indexes_tracker.index(position_index))  # removing position from the list
+                free_indexes_tracker.pop(free_indexes_tracker.index(position_index))  # removing position
+                # from the list
                 random_position_from_tracker = random.choice(free_indexes_tracker)  # picking a random position
                 # for opponent
                 board_list[random_position_from_tracker] = p_red(opponent_sign)
@@ -116,11 +136,3 @@ def play():
 
 
 play()
-
-
-
-
-
-
-
-
